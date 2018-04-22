fa78a16eeb354af9e032ec03dc1faf084a7f49ae
Munim <munim@celltron.com.bd>
2018-04-22 17:11:31 +0600

    Improved PEP8 issues

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 def add(a, b):
     return a+b
 
+
 def minus(a, b):
     return a-b
+
 
 def main():
     print("Welcome to Python")
     print("This is just a test")
     return "OK"
-    
-if __name__== "__main__":
+
+
+if __name__ == "__main__":
     main()
--- test_app.py
+++ test_app.py
@@ -1,12 +1,15 @@
 from app import add, minus, main
 
+
 def test_add():
-    assert add(1,2)==3
-    assert add(4,2)==6
+    assert add(1, 2) == 3
+    assert add(4, 2) == 6
+
 
 def test_minus():
-    assert minus(4,2)==2
-    assert minus(7,2)==5
+    assert minus(4, 2) == 2
+    assert minus(7, 2) == 5
+
 
 def test_main():
-    assert main()=='OK'
+    assert main() == 'OK'
